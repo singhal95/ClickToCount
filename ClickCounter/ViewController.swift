1b7b1d340c1c3bce813ca835ce19a8d5d7504db1
NITIN SINGHAL <singhal95nitin@gmail.com>
2018-04-11 23:42:27 +0530

    button and label added

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -13,6 +13,16 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let label=UILabel()
+        label.frame=CGRect(x: 150, y: 150, width: 60, height: 60)
+        label.text="0"
+        view.addSubview(label)
+        
+        let button=UIButton()
+        button.frame=CGRect(x: 150, y: 250, width: 60, height: 60)
+        button.setTitle("CLICK", for: .normal)
+        button.setTitleColor(UIColor.blue, for: .normal)
+        view.addSubview(button)
     }
 
     override func didReceiveMemoryWarning() {
