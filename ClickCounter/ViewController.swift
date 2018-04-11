c160adf31bca339a0a45f9ad4fd12388ad1e73e0
NITIN SINGHAL <singhal95nitin@gmail.com>
2018-04-12 00:01:38 +0530

    button functionality added

--- ClickCounter/ViewController.swift
+++ ClickCounter/ViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var count=0
+    var label : UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,17 +20,24 @@ class ViewController: UIViewController {
         label.frame=CGRect(x: 150, y: 150, width: 60, height: 60)
         label.text="0"
         view.addSubview(label)
-        
+        self.label=label
         let button=UIButton()
         button.frame=CGRect(x: 150, y: 250, width: 60, height: 60)
         button.setTitle("CLICK", for: .normal)
         button.setTitleColor(UIColor.blue, for: .normal)
         view.addSubview(button)
+        button.addTarget(self, action: #selector(ViewController.incrementCount), for: UIControlEvents.touchUpInside)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+   @objc func incrementCount(){
+        self.count+=1
+        self.label.text="\(self.count)"
+        
     }
 
 
